452509821a5dfdd19b6b71c718bc4943fdad8c4e
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2023-01-19 16:04:51 -0800

    mongo exercise1

--- mongo/solution1.js
+++ mongo/solution1.js
@@ -1,25 +1,28 @@
-const mongoose = require('mongoose');
+const { default: mongoose } = require("mongoose");
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mongo-excersices")
+  .then(() => console.log("Connected to Mongodb"))
+  .catch((err) => console.error("Error connection", err));
 
-mongoose.connect('mongodb://localhost/mongo-exercises');
-
+//Schema
 const courseSchema = new mongoose.Schema({
   name: String,
-  author: String, 
-  tags: [ String ],
-  date: Date, 
+  author: String,
+  tags: [String],
+  date: Date,
   isPublished: Boolean,
-  price: Number
+  price: Number,
 });
 
-const Course = mongoose.model('Course', courseSchema);
+//Model
+const Course = mongoose.model("Course", courseSchema);
 
 async function getCourses() {
-  return await Course
-  .find({ isPublished: true, tags: 'backend' })
-  .sort({ name: 1 })
-  .select({ name: 1, author: 1 });
+  return await Course.find({ isPublished: true })
+    .sort({ name: 1 })
+    .select({ name: 1, author: 1 });
 }
-
 async function run() {
   const courses = await getCourses();
   console.log(courses);
