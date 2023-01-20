d14abdc929f9441f6d7866d0a2f7e3b7f9fb0e15
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2023-01-19 20:36:41 -0800

    crud operations 1

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -8,11 +8,36 @@ mongoose
 
 //Schema
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true, minlength: 5, maxlength: 255 },
+  category: {
+    type: String,
+    enum: ["web", "mobile", "network"],
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function (v, callback) {
+        setTimeout(() => {
+          //Do some async work
+          const result = v && v.length > 0;
+          callback(result);
+        }, 1000);
+      },
+      message: "A course shoud have at least one tag.",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+  },
 });
 
 //
@@ -22,12 +47,19 @@ const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
     name: "Angular Course",
+    category: "web",
     author: "Fvasquez",
-    tags: ["Angular", "frontend"],
+    tags: [],
     isPublished: true,
+    price: 15,
   });
-  const result = await course.save();
-  console.log(result);
+  try {
+    await course.validate();
+    // const result = await course.save();
+    // console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 }
 
 async function getCourses() {
@@ -141,4 +173,4 @@ async function removeCourse(id) {
   console.log(result);
 }
 
-removeCourse("63c76fc1451dfcaaefedb03f");
+createCourse();
